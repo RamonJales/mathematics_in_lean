41ede2de49931c53a795b0eea1838cb5e819d825
RamonJales <ramonjales123@gmail.com>
2025-05-04 17:57:31 -0300

    mil-2.3 complete

--- MIL/C02_Basics/S03_Using_Theorems_and_Lemmas.lean
+++ MIL/C02_Basics/S03_Using_Theorems_and_Lemmas.lean
@@ -44,7 +44,10 @@ example (x : ℝ) : x ≤ x :=
 
 -- Try this.
 example (h₀ : a ≤ b) (h₁ : b < c) (h₂ : c ≤ d) (h₃ : d < e) : a < e := by
-  sorry
+  apply lt_of_le_of_lt h₀
+  apply lt_trans h₁
+  apply lt_of_le_of_lt h₂
+  exact h₃
 
 example (h₀ : a ≤ b) (h₁ : b < c) (h₂ : c ≤ d) (h₃ : d < e) : a < e := by
   linarith
@@ -86,23 +89,36 @@ example (h₀ : a ≤ b) (h₁ : c < d) : a + exp c + e < b + exp d + e := by
     apply exp_lt_exp.mpr h₁
   apply le_refl
 
-example (h₀ : d ≤ e) : c + exp (a + d) ≤ c + exp (a + e) := by sorry
+example (h₀ : d ≤ e) : c + exp (a + d) ≤ c + exp (a + e) := by
+  -- apply é backward
+  apply add_le_add_left
+  apply exp_le_exp.mpr
+  -- rw [exp_le_exp]
+  apply add_le_add_left h₀
+
 
 example : (0 : ℝ) < 1 := by norm_num
 
 example (h : a ≤ b) : log (1 + exp a) ≤ log (1 + exp b) := by
-  have h₀ : 0 < 1 + exp a := by sorry
+  have h₀ : 0 < 1 + exp a := by
+    apply add_pos
+    norm_num
+    apply exp_pos a
   apply log_le_log h₀
-  sorry
+  apply add_le_add_left
+  apply exp_le_exp.mpr h
 
 example : 0 ≤ a ^ 2 := by
   -- apply?
   exact sq_nonneg a
 
 example (h : a ≤ b) : c - exp b ≤ c - exp a := by
-  sorry
+  -- apply sub_le_sub_left
+  apply add_le_add_left
+  apply neg_le_neg
+  apply exp_le_exp.mpr h
 
-example : 2*a*b ≤ a^2 + b^2 := by
+theorem theorem1 : 2*a*b ≤ a^2 + b^2 := by
   have h : 0 ≤ a^2 - 2*a*b + b^2
   calc
     a^2 - 2*a*b + b^2 = (a - b)^2 := by ring
@@ -120,8 +136,21 @@ example : 2*a*b ≤ a^2 + b^2 := by
     _ ≥ 0 := by apply pow_two_nonneg
   linarith
 
+theorem theorem2 : -(a*b)*2 ≤ a^2 + b^2 := by
+  have h : 0 ≤ a^2 + 2*a*b + b^2
+  calc
+    a^2 + 2*a*b + b^2 = (a + b)^2 := by ring
+    _ ≥ 0 := by apply pow_two_nonneg
+  linarith
+
 example : |a*b| ≤ (a^2 + b^2)/2 := by
-  sorry
+  have h : (0 : ℝ) < 2 := by norm_num
+  apply abs_le'.mpr
+  constructor
+  rw [le_div_iff₀ h]
+  rw[mul_assoc, mul_comm, mul_comm b, mul_assoc, mul_comm b, ← mul_assoc]
+  apply theorem1
+  rw [le_div_iff₀ h]
+  apply theorem2
 
 #check abs_le'.mpr
-
