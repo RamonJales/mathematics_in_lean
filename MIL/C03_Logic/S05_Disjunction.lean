9ab0cb79b115ec81cf0db5a4c9b7cefe2607f743
RamonJales <ramonjales123@gmail.com>
2025-06-01 15:52:06 -0300

    mil-3.5

--- MIL/C03_Logic/S05_Disjunction.lean
+++ MIL/C03_Logic/S05_Disjunction.lean
@@ -58,19 +58,68 @@ example : x < |y| → x < y ∨ x < -y := by
 namespace MyAbs
 
 theorem le_abs_self (x : ℝ) : x ≤ |x| := by
-  sorry
+  cases le_or_gt 0 x
+  next h =>
+    rw [abs_of_nonneg]
+    apply h
+  next h =>
+    rw [abs_of_neg h]
+    linarith
 
 theorem neg_le_abs_self (x : ℝ) : -x ≤ |x| := by
-  sorry
+  rcases le_or_gt 0 x with h | h --ganho hipoteses pela definição de abs
+  rw [abs_of_nonneg]
+  linarith
+  exact h
+  rw [abs_of_neg]
+  exact h
 
 theorem abs_add (x y : ℝ) : |x + y| ≤ |x| + |y| := by
-  sorry
+  rcases le_or_gt 0 (x+y) with h | h
+  rw [abs_of_nonneg h]
+  linarith [le_abs_self x, le_abs_self y]
+  rw [abs_of_neg h]
+  linarith [neg_le_abs_self x, neg_le_abs_self y]
 
 theorem lt_abs : x < |y| ↔ x < y ∨ x < -y := by
-  sorry
+  rcases le_or_gt 0 y with h | h
+  rw [abs_of_nonneg h]
+  constructor
+  intro h1
+  left
+  exact h1
+  intro h1
+  rcases h1 with h' | h'
+  exact h'
+  linarith
+  rw [abs_of_neg h]
+  constructor
+  intro h1
+  right
+  exact h1
+  intro h1
+  rcases h1 with h' | h'
+  linarith
+  exact h'
 
 theorem abs_lt : |x| < y ↔ -y < x ∧ x < y := by
-  sorry
+  rcases le_or_gt 0 x with h | h
+  rw [abs_of_nonneg h]
+  constructor
+  intro h1
+  constructor
+  linarith
+  exact h1
+  intro h1
+  exact h1.right
+  rw [abs_of_neg h]
+  constructor
+  intro h1
+  constructor
+  linarith
+  linarith
+  intro h1
+  linarith
 
 end MyAbs
 
@@ -91,23 +140,66 @@ example {m n k : ℕ} (h : m ∣ n ∨ m ∣ k) : m ∣ n * k := by
     apply dvd_mul_right
 
 example {z : ℝ} (h : ∃ x y, z = x ^ 2 + y ^ 2 ∨ z = x ^ 2 + y ^ 2 + 1) : z ≥ 0 := by
-  sorry
+  rcases h with ⟨x, y, hz⟩
+  rcases hz with rfl | rfl
+  · -- caso z = x^2 + y^2
+    apply add_nonneg
+    apply sq_nonneg
+    apply sq_nonneg
+  · -- caso z = x^2 + y^2 + 1
+    apply add_nonneg
+    apply add_nonneg
+    apply sq_nonneg
+    apply sq_nonneg
+    norm_num
 
 example {x : ℝ} (h : x ^ 2 = 1) : x = 1 ∨ x = -1 := by
-  sorry
+  have h' : x ^ 2 - 1 = 0 := by rw [h, sub_self]
+  have h'' : (x + 1) * (x - 1) = 0 := by
+    rw [← h']
+    ring
+  rcases eq_zero_or_eq_zero_of_mul_eq_zero h'' with h1 | h1
+  · right
+    exact eq_neg_iff_add_eq_zero.mpr h1
+  · left
+    exact eq_of_sub_eq_zero h1
 
 example {x y : ℝ} (h : x ^ 2 = y ^ 2) : x = y ∨ x = -y := by
-  sorry
+  have h' : x ^ 2 - y ^ 2 = 0 := by rw [h, sub_self]
+  have h'' : (x + y) * (x - y) = 0 := by
+    rw [← h']
+    ring
+  rcases eq_zero_or_eq_zero_of_mul_eq_zero h'' with h1 | h1
+  · right
+    exact eq_neg_iff_add_eq_zero.mpr h1
+  · left
+    exact eq_of_sub_eq_zero h1
 
 section
 variable {R : Type*} [CommRing R] [IsDomain R]
 variable (x y : R)
 
 example (h : x ^ 2 = 1) : x = 1 ∨ x = -1 := by
-  sorry
+  have h' : x ^ 2 - 1 = 0 := by rw [h, sub_self]
+  have h'' : (x + 1) * (x - 1) = 0 := by
+    rw [← h']
+    ring
+  rcases eq_zero_or_eq_zero_of_mul_eq_zero h'' with h1 | h1
+  · right
+    exact eq_neg_iff_add_eq_zero.mpr h1
+  · left
+    exact eq_of_sub_eq_zero h1
 
 example (h : x ^ 2 = y ^ 2) : x = y ∨ x = -y := by
-  sorry
+  have h' : x ^ 2 - y ^ 2 = 0 := by rw [h, sub_self]
+  have h'' : (x + y) * (x - y) = 0 := by
+    rw [← h']
+    ring
+  rcases eq_zero_or_eq_zero_of_mul_eq_zero h'' with h1 | h1
+  · right
+    exact eq_neg_iff_add_eq_zero.mpr h1
+  · left
+    exact eq_of_sub_eq_zero h1
 
 end
 
@@ -124,5 +216,15 @@ example (P : Prop) : ¬¬P → P := by
   contradiction
 
 example (P Q : Prop) : P → Q ↔ ¬P ∨ Q := by
-  sorry
-
+  constructor
+  intro h
+  by_cases  h1 : P
+  right
+  apply h
+  apply h1
+  left
+  apply h1
+  intro h1 h2
+  rcases h1 with h' | h'
+  contradiction
+  apply h'
